abcab19dd8df2c9e5f7614946ab77200754c253e
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2025-06-03 07:54:17 +0700

    Unfinished JWT

---
+++ Business/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public interface IUserService
+    {
+        Task<User> Login(string Email, string password);
+    }
+}
---
+++ Business/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using BusinessObject.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly SchoolMedicalDbContext _context;
+
+        public UserService(SchoolMedicalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Login(string Email, string password)
+        {
+            var account = await _context.Users.FirstOrDefaultAsync(account => account.Email == Email && account.PasswordHash == password);
+            return account;
+        }
+    }
+}
