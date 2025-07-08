324c803c898a214f863693d47a03ff18906b1565
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2025-07-08 16:49:30 +0700

    Update AutoMapper.cs

--- Business/Mapper/AutoMapper.cs
+++ Business/Mapper/AutoMapper.cs
@@ -13,9 +13,35 @@ namespace BusinessLogic.Mapper
     {
         public AutoMapperProfile()
         {
+            // Parent
             CreateMap<Parent, ParentResponseDTO>();
             CreateMap<ParentRequestDTO, Parent>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // Student
+            CreateMap<Student, StudentResponseDTO>();
+            CreateMap<StudentRequestDTO, Student>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<Student, BusinessLogic.DTOs.Student.StudentDto>();
+            CreateMap<BusinessLogic.DTOs.Student.StudentDto, Student>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // SchoolNurse
+            CreateMap<SchoolNurse, SchoolNurseResponseDTO>();
+            CreateMap<SchoolNurseRequestDTO, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<SchoolNurse, BusinessLogic.DTOs.SchoolNurse.SchoolNurseDto>();
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.SchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.CreateSchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.UpdateSchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // Login
+            CreateMap<LoginRequest, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<User, LoginResponseDTO>();
         }
     }
 }
