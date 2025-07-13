78ba3b44c3e3d6d723c6a16bac810cb1c38413ac
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2025-07-13 16:57:49 +0700

    Fixed merge

--- BusinessObject/Entity/SchoolNurse.cs
+++ BusinessObject/Entity/SchoolNurse.cs
@@ -10,10 +10,6 @@ namespace BusinessObject.Entity
 {
     public class SchoolNurse
     {
-<<<<<<< Updated upstream
-=======
-        [Key]
->>>>>>> Stashed changes
         public int Id { get; set; }
         public string FullName { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
