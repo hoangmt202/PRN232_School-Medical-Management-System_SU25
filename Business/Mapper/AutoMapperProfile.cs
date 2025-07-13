817db2e8307cd4de47ec62cc33bd2b890a8dda23
hungmegame123fpt <hungthinh26072004@gmail.com>
2025-07-13 15:13:27 +0700

    .

--- Business/Mapper/AutoMapperProfile.cs
+++ Business/Mapper/AutoMapperProfile.cs
@@ -27,6 +27,35 @@ namespace BusinessLogic.Mapper
             CreateMap<InventoryReport, InventoryReportDto>().ReverseMap();
             CreateMap<VaccinationPlan, VaccinationPlanDto>().ReverseMap();
             CreateMap<Vaccination, VaccinationRecordDto>().ReverseMap();
+
+            CreateMap<Parent, ParentResponseDTO>();
+            CreateMap<ParentRequestDTO, Parent>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // Student
+            CreateMap<Student, StudentResponseDTO>();
+            CreateMap<StudentRequestDTO, Student>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<Student, BusinessLogic.DTOs.Student.StudentDto>();
+            CreateMap<BusinessLogic.DTOs.Student.StudentDto, Student>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // SchoolNurse
+            CreateMap<SchoolNurse, SchoolNurseResponseDTO>();
+            CreateMap<SchoolNurseRequestDTO, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<SchoolNurse, BusinessLogic.DTOs.SchoolNurse.SchoolNurseDto>();
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.SchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.CreateSchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<BusinessLogic.DTOs.SchoolNurse.UpdateSchoolNurseDto, SchoolNurse>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            // Login
+            CreateMap<LoginRequest, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<User, LoginResponseDTO>();
         }
     }
 }
